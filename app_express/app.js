88aa11c68ec5d2e307d0b00d345dea9e038640a3
Jerome Lin <jeromelin@axvs-MacBook-Pro-2.local>
2024-04-27 17:41:09 +0800

    Add Prometheus metrics to Express app and Dockerize
    - Integrate express-prometheus-middleware to generate runtime metrics for the Express application.
    
    - Create a Dockerfile for containerizing the Express app, ensuring it's ready for deployment.
    
    - Include the Express app as a service in the Docker Compose configuration to streamline local development and deployment processes.

---
+++ app_express/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const promMid = require('express-prometheus-middleware')
+const app = express()
+
+const port = 8080
+
+app.use(promMid({
+    metricsPath: '/metric',
+    collectDefaultMetrics: true,
+    requestDurationBuckets: [0.1, 0.5, 1, 1.5],
+    requestLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
+    responseLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400]
+}))
+
+app.get('/', (req, res) => {
+    console.log('Get express /')
+    res.json({message: 'Express app with prometheus metrics.'})
+})
+
+app.listen(port, () => {
+    console.log(`express server is running on http://localhost:${port}`)
+})
